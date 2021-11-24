92771c47884cf745bd898352dedcb032ac31d52b
Mr. Quantum <36820091+mrquantumoff@users.noreply.github.com>
2021-11-24 20:48:54 +0200

    Update main.cs

--- SharpPackageManager/main.cs
+++ SharpPackageManager/main.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 public class SharpPackageManager
 {
     public static int latestversion;
-    public static int currentversion = 3;
+    public static int currentversion =  4;
     public static string curbranch = "ptb";
     public static string tag;
     public static List<String> reponames = new List<String>();
