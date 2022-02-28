f3f70eabb2ef2e532bf166e1347c1666ea690fc5
Mr. Quantum <demiryerli@gmail.com>
2022-02-28 17:54:50 +0300

    Update libspm.py

--- libspm.py
+++ libspm.py
@@ -2,8 +2,16 @@ import os
 import shutil
 import argparse
 libspm = False
-version=0.1
+version=0.2
 def main():
     if os.path.exists("C:\\SPM\\modules"):
         libspm = True
+def RmFile(filename):
+    os.remove(filename)
+def CpFile(filename, dest):
+    os.copy(filename, dest)
+def MkFile(filename):
+    os.touch(filename)
+def MkDir(directory):
+    os.mkdir(directory)
 main();
