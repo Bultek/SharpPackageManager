004aacb94f65f42d696a7630d7e4d7962472fbe1
Mr. Quantum <demiryerli@gmail.com>
2022-03-11 13:02:03 +0300

    Update main.cs

--- SharpPackageManager/main.cs
+++ SharpPackageManager/main.cs
@@ -1,17 +1,16 @@
-using System;
+﻿using System;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Collections.Generic;
 using System.Diagnostics;
 #pragma warning disable
-
 public class SharpPackageManager
 {
     public static bool AreModulesLoaded = false;
     public static int latestversion;
-    public static int currentversion =  21;
-    public static string appversion = "v2.2.0 - PTB-1";
+    public static int currentversion =  22;
+    public static string appversion = "v2.2.0 - PTB-2";
     public static string curbranch = "ptb";
 
     public static string? tag;
@@ -688,7 +687,7 @@ public class SharpPackageManager
                         System.IO.File.AppendAllText(File, data);
                         break; // When done we can break loop
                     }
-                    catch (IOException e) when (i <= 3) {
+                    catch (IOException) when (i <= 3) {
                     // You may check error code to filter some exceptions, not every error
                     // can be recovered.
                     Thread.Sleep(1000);
