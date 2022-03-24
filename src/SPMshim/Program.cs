f6603a6ca00aa8646b788b1449ce2a9aab18649e
Demir Yerli <demiryerli@gmail.com>
2022-03-24 10:51:02 +0300

    start work on shims, make source code easier to understand

---
+++ src/SPMshim/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+namespace SharpPackageManager {
+
+
+    public class ShimInfo {
+        public string Name { get; set; }
+        public string Executable { get; set; }
+        public string Icon { get; set; }
+    }
+    public class Shim {
+
+        public static void Main(string[] args) {
+            ShimInfo info = new ShimInfo();
+            info.Name = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            info.Executable = @"C:\SPM-APPS\"+info.Name+@"\"+info.Name+@".exe";
+            info.Icon = @"C:\SPM-APPS\"+info.Name+@"\icon.ico";
+            Console.Title = info.Name;
+            Debug.WriteLine(info.Executable, info.Icon, info.Name);     
+        }
+    }
+}
